9c51a4c89b59924d6f768a352fba4f0341df3ea1
Quintus <quintus@quintilianus.eu>
2014-06-26 22:14:09 +0200

    Allow submit of settings form

--- app/controllers/settings.rb
+++ app/controllers/settings.rb
@@ -13,6 +13,22 @@ Chessboard::App.controllers :settings do
     render "settings/main"
   end
 
+  patch :main, :map => "/settings/main" do
+    @settings = env["warden"].user.settings
+
+    @settings.hide_status               = params["settings"]["hide_status"]
+    @settings.language                  = params["settings"]["language"]
+    @settings.preferred_markup_language = params["settings"]["preferred_markup_language"]
+    @settings.time_format               = params["settings"]["time_format"]
+
+    if @settings.save
+      flash[:notice] = "Settings updated."
+      redirect "/settings/main"
+    else
+      render "settings/main"
+    end
+  end
+
   get :avatar, :map => "/settings/avatar" do
     render "settings/avatar"
   end
