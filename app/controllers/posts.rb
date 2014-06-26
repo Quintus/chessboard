d98b9f93545765856148b42c0381632ff48a327b
Quintus <quintus@quintilianus.eu>
2014-06-26 11:26:11 +0200

    Add posts controller

---
+++ app/controllers/posts.rb
@@ -0,0 +1,7 @@
+Chessboard::App.controllers :posts do
+  
+  get :new, :map => "/posts/new" do
+    # TODO
+  end
+
+end
---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Chessboard
+  class App
+    module PostsHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers PostsHelper
+  end
+end
