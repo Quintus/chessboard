ecc6416349a2fd5e7a63a88d4571ad6304405e68
Quintus <quintus@quintilianus.eu>
2014-06-27 19:09:41 +0200

    Add reply form

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,7 +1,31 @@
 Chessboard::App.controllers :posts do
-  
-  get :new, :map => "/posts/new" do
-    # TODO
+
+  before do
+    env["warden"].authenticate!
+  end
+
+  get :new, :map => "/topics/:topic_id/posts/new" do
+    @post = Post.new
+    @topic = Topic.find(params["topic_id"])
+    render "posts/new"
+  end
+
+  post :create, :map => "/topics/:topic_id/posts/new" do
+    @post = Post.new(params["post"])
+    @post.topic = Topic.find(params["topic_id"])
+    @post.author = env["warden"].user
+
+    if request.xhr?
+      raise "TODO"
+    else
+      if @post.save
+        flash[:notice] = "Post created."
+        redirect url(:topics, :show, @post.topic.id) + "#p#{@post.id}"
+      else
+        @topic = @post.topic
+        render "posts/new"
+      end
+    end
   end
 
 end
