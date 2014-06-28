bdfe1d43af535f4d4e405a423ffcc4c07efbcb0f
Quintus <quintus@quintilianus.eu>
2014-06-28 13:09:08 +0200

    Add possibility to create sticky/announce topics

--- app/controllers/topics.rb
+++ app/controllers/topics.rb
@@ -5,8 +5,11 @@ Chessboard::App.controllers :topics do
   end
 
   get :new, :map => "/topics/new" do
+    halt 400 unless params["forum"] # query parameter required!
+
     @topic = Topic.new
     @topic.posts.build(:markup_language => env["warden"].user.settings.preferred_markup_language)
+    @forum = Forum.find(params["forum"])
     render "new"
   end
 
@@ -26,6 +29,10 @@ Chessboard::App.controllers :topics do
     @topic.forum = Forum.find(params["topic"]["forum_id"])
     @topic.author = env["warden"].user
 
+    # Only admins/moderators can enable these
+    @topic.sticky = params["topic"]["sticky"] == "1" if env["warden"].user.admin? || env["warden"].user.moderates?(@topic.forum)
+    @topic.announcement = params["topic"]["announcement"] == "1" if env["warden"].user.admin?
+
     initial_post = Post.new(params["topic"]["posts_attributes"]["0"])
     initial_post.author = env["warden"].user
     @topic.posts << initial_post
@@ -34,7 +41,7 @@ Chessboard::App.controllers :topics do
       flash[:notice] = "Topic created"
       redirect url(:topics, :show, @topic.id)
     else
-      @forum_id = params["topic"]["forum_id"]
+      @forum = Forum.find(params["topic"]["forum_id"])
       render "new"
     end
   end
