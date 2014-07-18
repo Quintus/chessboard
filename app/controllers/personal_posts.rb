821c027c272f227be65ca3053703fd3f178a2fec
Quintus <quintus@quintilianus.eu>
2014-07-18 15:48:38 +0200

    Allow PM post editing

--- app/controllers/personal_posts.rb
+++ app/controllers/personal_posts.rb
@@ -15,6 +15,8 @@ Chessboard::App.controllers :personal_posts do
     @post.personal_message = PersonalMessage.find(params["id"])
     @post.author = env["warden"].user
 
+    halt 403 unless @post.personal_message.allowed_users.include?(env["warden"].user)
+
     if @post.save
       flash[:notice] = I18n.t("posts.created")
       redirect ppost_url(@post)
@@ -24,9 +26,24 @@ Chessboard::App.controllers :personal_posts do
   end
 
   get :edit, :map => "/pms/:pm_id/posts/:id/edit" do
+    @post = PersonalPost.find(params["id"])
+    halt 403 unless @post.author == env["warden"].user
+    halt 403 unless @post.personal_message.allowed_users.include?(env["warden"].user) # author may have been removed from allowed users in the meanwhile
+
+    render "edit"
   end
 
   patch :update, :map => "/pms/:pm_id/posts/:id" do
+    @post = PersonalPost.find(params["id"])
+    halt 403 unless @post.author == env["warden"].user
+    halt 403 unless @post.personal_message.allowed_users.include?(env["warden"].user) # author may have been removed from allowed users in the meanwhile
+
+    if @post.update_attributes(params["personal_post"])
+      flash[:notice] = I18n.t("pms.edited_post")
+      redirect ppost_url(@post)
+    else
+      render "edit"
+    end
   end
 
   delete :destroy, :map => "/pms/:pm_id/posts/:id" do
