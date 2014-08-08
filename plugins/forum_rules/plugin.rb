e14a5f02370d06c0a211aaa4345753d8b06c6682
Quintus <quintus@quintilianus.eu>
2014-08-08 16:50:48 +0200

    Require acceptance of forum rules on registration

--- plugins/forum_rules/plugin.rb
+++ plugins/forum_rules/plugin.rb
@@ -1,3 +1,24 @@
+# = Forum rules plugin
+#
+# This plugin adds a link to the top menu referencing a new
+# page "Forum rules" that contains a free configurable text.
+# It also injects an unticked checkbox "accept rules" into
+# the registration form that the user has to tick in order
+# to complete the registration.
+#
+# == Configuration
+# Add the following to your settings.rb:
+#
+#   config.plugins.ForumRulesPlugin = {
+#     :rules => <<-RULES
+# <h1>Forum rules</h1>
+# <p>Your rules here.</p>
+#     RULES
+#   }
+#
+# === Keywords
+# [rules]
+#   Free-form HTML for your rules page.
 module ForumRulesPlugin
   include Chessboard::Plugin
 
@@ -17,6 +38,17 @@ module ForumRulesPlugin
     end
 
     content
+  end
+
+  def hook_ctrl_registration(options)
+    return false unless super
+
+    if options[:params]["accept_forum_rules"].to_i != 1
+      options[:user].errors[:base] << I18n.t("plugins.forum_rules.must_accept")
+      return false
+    end
+
+    true
   end
 
   def hook_layout_navigation(options)
