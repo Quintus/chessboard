a07e0213c09fc7993ba3ba1419d7bdb3cf03b003
Quintus <quintus@quintilianus.eu>
2014-09-01 18:44:44 +0200

    Change Forum#categorized_topics to return a hash

--- app/controllers/forums.rb
+++ app/controllers/forums.rb
@@ -1,24 +1,5 @@
 Chessboard::App.controllers :forums do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
 
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   'Maps to url '/foo/#{params[:id]}''
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
   get :index, :map => "/forums" do
     @forum_groups = ForumGroup.order(:name => :asc)
     render "index"
--- models/forum.rb
+++ models/forum.rb
@@ -6,20 +6,31 @@ class Forum < ActiveRecord::Base
   belongs_to :forum_group
   has_many :topics
 
-  # Returns all topics for this forum plus any announcements, in
-  #  this order:
-  # 1. All announcements, ordered reversely by update
-  # 2. All stickies for this forum, ordered reversely by update
-  # 3. All normal topics for this forum, ordered reversely by update
-  def categorized_topics
+  # Returns all topics for this forum plus any announcements, as
+  # a hash with the following keys:
+  #
+  # [:announcements]
+  #   All announcements, ordered reversely by update
+  # [:stickies]
+  #   All stickies for this forum, ordered reversely by update
+  # [:normal]
+  #   All normal topics for this forum, ordered reversely by update
+  #
+  # +offset+ and +limit+ parameters are applied only to the
+  # the SQL query for the +normal+ topics.
+  def categorized_topics(offset = nil, limit = nil)
+    result = {}
+
     # First announcements
-    result = Topic.where(:announcement => true).joins(:posts).group("topics.id").order("MAX(posts.updated_at) DESC")
+    result[:announcements] = Topic.where(:announcement => true).joins(:posts).group("topics.id").order("MAX(posts.updated_at) DESC")
 
     # Next sticky topics
-    result += topics.where(:sticky => true).joins(:posts).group("topics.id").order("MAX(posts.updated_at) DESC")
+    result[:stickies] = topics.where(:sticky => true).joins(:posts).group("topics.id").order("MAX(posts.updated_at) DESC")
 
     # Finally normal topics
-    result += topics.where(:sticky => false, :announcement => false).joins(:posts).group("topics.id").order("MAX(posts.updated_at) DESC")
+    result[:normal] = topics.where(:sticky => false, :announcement => false).joins(:posts).group("topics.id").order("MAX(posts.updated_at) DESC")
+    result[:normal] = result[:normal].offset(offset) if offset
+    result[:normal] = result[:normal].limit(limit) if limit
 
     # Result
     result
