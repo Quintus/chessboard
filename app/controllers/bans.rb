29c349cd200aac602e6c002b44c7e1377d2ff765
Quintus <quintus@quintilianus.eu>
2015-06-20 19:11:50 +0200

    Add banning actions to the moderation log

--- app/controllers/bans.rb
+++ app/controllers/bans.rb
@@ -1,5 +1,5 @@
 Chessboard::App.controllers :bans do
-  
+
   get :index, :map => "/moderation/bans" do
     @bans = Ban.order(:created_at => :desc)
     render "moderation/bans/index"
@@ -15,6 +15,7 @@ Chessboard::App.controllers :bans do
 
     if @ban.save
       flash[:notice] = I18n.t("bans.created")
+      create_modlog_entry(@ban, :create)
       redirect url(:bans, :index)
     else
       render "moderation/bans/new"
@@ -31,6 +32,7 @@ Chessboard::App.controllers :bans do
 
     if @ban.update_attributes(params["ban"])
       flash[:notice] = I18n.t("bans.updated")
+      create_modlog_entry(@ban, :update)
       redirect url(:bans, :index)
     else
       render "moderation/bans/edit"
@@ -41,8 +43,34 @@ Chessboard::App.controllers :bans do
     @ban = Ban.find(params["id"])
     @ban.destroy
 
+    create_modlog_entry(@ban, :destroy)
+
     flash[:notice] = I18n.t("bans.deleted")
     redirect url(:bans, :index)
   end
 
+end
+
+def create_modlog_entry(ban, modification)
+  ary = []
+  ary << "nick=#{ban.nick_pattern}"   unless ban.nick_pattern.blank?
+  ary << "email=#{ban.email_pattern}" unless ban.email_pattern.blank?
+  ary << "ip=#{ban.ip_range}"         unless ban.ip_range.blank?
+
+  if modification == :create
+    action = "Cast a ban on #{ary.join('/')} that will"
+    action += ban.expiration_date? ? "expire on #{ban.expiration_date}" : "never expire"
+    action += ". #{ban.reason}"
+  elsif modification == :update
+    action = "Modified the ban on #{ary.join('/')} that will"
+    action += ban.expiration_date? ? "expire on #{ban.expiration_date}" : "never expire"
+    action += ". #{ban.reason}"
+  elsif modification == :destroy
+    action = "Released the ban on #{ary.join('/')}."
+  else
+    logger.error "Invalid modlog entry request in bans.rb: #{modification} #{ban.inspect}"
+    return
+  end
+
+  Moderation.create(:moderator => env["warden"].user, :action => action)
 end
