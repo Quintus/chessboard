b06969f19421ab33cf750d29d3fe583489266b63
Quintus <quintus@quintilianus.eu>
2016-08-11 19:09:00 +0200

    Add Expand-all and Collapse-all buttons to thread view.

--- public/javascripts/thread.js
+++ public/javascripts/thread.js
@@ -3,4 +3,20 @@ $(document).ready(function(){
 	$(this).parent().children(".intree-post").toggle();
 	return false;
     });
+
+    $(".expand-all a").click(function(){
+	$(".intree-post").show();
+	$(".expand-all").hide();
+	$(".collapse-all").show();
+	return false;
+    });
+
+    $(".collapse-all a").click(function(){
+	$(".intree-post").hide();
+	$(".collapse-all").hide();
+	$(".expand-all").show();
+	return false;
+    });
+
+    $(".expand-all").show();
 });
